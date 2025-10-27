f20f0d18f63748b73aedc927a45deb7027c9a9a1
leviegu <leviegu@gmail.com>
2025-10-27 22:52:17 +0800

    Temp prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,30 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  objectWrap: "preserve",
+  bracketSpacing: true,
+  semi: false,
+  experimentalOperatorPosition: "end",
+  experimentalTernaries: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  singleAttributePerLine: true,
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  proseWrap: "preserve",
+  insertPragma: false,
+  requirePragma: false,
+  tabWidth: 2,
+  useTabs: false,
+  embeddedLanguageFormatting: "auto",
+  printWidth: 120,
+  plugins: ["@prettier/plugin-oxc"],
+}
+
+export default config
---
+++ packages/dashboard/rsbuild.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "@rsbuild/core"
+import { pluginBabel } from "@rsbuild/plugin-babel"
+import { pluginReact } from "@rsbuild/plugin-react"
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions(opts) {
+        opts.plugins?.unshift("babel-plugin-react-compiler")
+      },
+    }),
+  ],
+  tools: {
+    postcss: (_, { addPlugins }) => {
+      addPlugins(require("@tailwindcss/postcss"))
+    },
+  },
+})
