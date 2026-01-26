00cc176aa59e48156abe5b3fae6072828d7bdc1a
leviegu <leviegu@gmail.com>
2026-01-26 22:58:45 +0800

    feat(finances-api): add new Go service with Gin framework

---
+++ packages/finances-api/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	// listens on 0.0.0.0:8080 by default
+	router.Run()
+}
